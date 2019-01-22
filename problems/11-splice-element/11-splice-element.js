08deafb2611d3b01988c0188ecf005f1b63d0f6e
andrewhsoftware <46683746+andrewhsoftware@users.noreply.github.com>
2019-01-22 09:41:46 -0500

    Add files via upload

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,49 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
-function spliceElement(someArr, index) {
-  
+// 0 is for remove, 1 is for add, 2 is for replace, since you don't have to use every parameter you can leave itemToAdd in there and it still works nicely
+function spliceElement(someArr, operatorCall, index, itemToAdd) { //remove item at index
+    let splicedArr = [];
+    if(operatorCall === 0){
+    for(let i = 0; i < someArr.length; i++){
+        
+        if(i !== index){
+            splicedArr.push(someArr[i]);
+        }
+    }
+    return splicedArr;
 }
+    if(operatorCall === 1) { //add item at index
+    let splicedArr = [];
+    for(let i = 0; i < someArr.length; i++){
+        if(i === index){
+            splicedArr.push(itemToAdd);
+        }
+        
+        splicedArr.push(someArr[i]);
+        
+    }
+    return splicedArr;
+}
+if(operatorCall === 2) { //replace item at index
+    let splicedArr = [];
+    for(let i = 0; i < someArr.length; i++){
+        if(i === index){
+            splicedArr.push(itemToAdd);
+        }
+        if(i !== index){
+        splicedArr.push(someArr[i]);
+        }
+    }
+    return splicedArr;
+
+}
+}
+let someArr = [0,1,2,3,4,5];
+
+let splicedArr = spliceElement(someArr,0,3);
+console.log(splicedArr);
+let splicedArr2 = spliceElement(someArr,1,3, 9);
+console.log(splicedArr2);
+let splicedArr3 = spliceElement(someArr,2,3,0);
+console.log(splicedArr3);
