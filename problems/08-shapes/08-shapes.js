c5fce8604f0e8cfcadc2858e584a0368834143f3
andrewhsoftware <46683746+andrewhsoftware@users.noreply.github.com>
2019-01-22 09:40:36 -0500

    Add files via upload

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,48 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  let characterString = "";
+  if(height > 0){
+  if(shape === "Square") {
+    for(i = 0;i < height; i++){
+      let characterString = "";
+      for(k = 0; k < height; k++){
+        characterString = characterString.concat(character);
+      }
+      console.log(characterString)
+    }
+  } else if(shape === "Triangle") {
+    let characterString = "";
+    for(i = 0;i < height; i++){
+        characterString = characterString.concat(character);
+        console.log(characterString)
+      }
+    } else if(shape === "Diamond") {
+      let str = '';
+    for(let i=1; i<=height; i++){
+        for(let k=1; k<=height-i; k++){
+            str += " ";
+        }
+        for(var j=1; j<=i; j++){
+            str += "* ";
+        }
+        console.log(str);
+        str = "";
+      }
+    str = '';
+    for(var i=height; i>=1; i--){
+        for(var k=height; k>=i; k--){
+            str += " ";
+            }
+            for(var j=i-1; j>=1; j--){
+                str += character+" ";
+            }
+            console.log(str);
+            str = "";
+        }
+    }
+  }
 }
+printShape("Square", 5, "*");
+printShape("Triangle",5,"*");
+printShape("Diamond",5,"*");
