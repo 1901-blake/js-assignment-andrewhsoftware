a878559f671e856ff03ef1837b21f5ca76d504b1
andrewhsoftware <46683746+andrewhsoftware@users.noreply.github.com>
2019-01-22 09:41:23 -0500

    Add files via upload

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,24 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let Animal = {
+    name: "Elephant",
+    age: "old"
+
+}
+function Elephant(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+class Person{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+      }
+}
+let thisElephant = new Elephant("dumbo", 20);
+let Andrew = new Person("Andrew",25);
+console.log(Animal);
+console.log(thisElephant);
+console.log(Andrew);
